ca7b6bc3fdf4adac0a034c15f55f8b0c7c2f7fa0
Vaishnav Srivastava <103301221+Vaishnav129f@users.noreply.github.com>
2023-09-16 21:05:17 +0000

    2hours

---
+++ datatypes-summary.js
@@ -0,0 +1,46 @@
+// Primitive 
+
+//7 types : String , Number, Boolean, null, undefined,Symbol(unique banane ke liye use krte component), BigINT
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail ;
+
+const id= Symbol('123');
+const anotherId = Symbol('123');
+
+// console.log(id === anotherId);
+
+//const bigNumber = 34565435565674557548n
+// Referenced (Non Primitive)
+
+//Array, objects ,Functions
+
+const heros = ["shaktiman", "naagraj","doga"];
+let myObj = {
+    name:"hitesh",
+    age: 22,
+
+};
+
+const myFunction = function () {
+    console.log("Hello World");
+}
+
+console.log(typeof id);
+
+
+//++++++++++++++++++++++++++++++++
+// Stack (Primitive),Heap (Non Primitive)
+
+let myYoutubename= "VaishnavSrivastava";
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+

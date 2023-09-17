3dd69cdbb308c9e835ac2a2c758650a3aa8783ed
Vaishnav Srivastava <103301221+Vaishnav129f@users.noreply.github.com>
2023-09-17 19:59:32 +0000

    string

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -44,3 +44,17 @@ anothername = "chaiaurcode"
 console.log(myYoutubename);
 console.log(anothername);
 
+let userOne = {
+    email:"user@google.com",
+    upi:"user@ybl"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+

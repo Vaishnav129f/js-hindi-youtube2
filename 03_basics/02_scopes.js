d5bcf820e5e8bf5db42eb6f37be077086d676905
Vaishnav Srivastava <103301221+Vaishnav129f@users.noreply.github.com>
2023-09-20 15:40:06 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+let a =10
+
+const b = 20
+
+var c =30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "vaishnav"
+
+    function two(){
+        const website ="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+one()
